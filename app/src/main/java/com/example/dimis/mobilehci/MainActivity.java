d2ebea09a9495e4ab7226467578434942fadf515
Dimis94 <eleftherioudimitris@gmail.com>
2018-02-18 22:37:37 +0000

    Initial activities

---
+++ app/src/main/java/com/example/dimis/mobilehci/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dimis.mobilehci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_screen);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return  true;
+    }
+}
